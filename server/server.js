caff5aab15fa03528ebe7b92f5361a9232ad45a8
akwong372 <akwong372@gmail.com>
2019-03-08 16:32:38 -0800

    Prepare for elastic beanstalk

--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
 var cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/restaurants/:restaurantId', express.static(path.join(__dirname, '../public')));
-// app.use('/restaurants/:restaurantId', express.static(path.join(__dirname, '../Restaurant_details/src')));
-
-// app.get('/restaurants/:restaurantId', (req,res)=>res.send(req.params.restaurantId))
 
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
+  console.log(`server running at port ${port}`);
 });
